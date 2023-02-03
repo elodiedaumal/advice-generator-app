22fdc32370205bc50aca61ff011cc0b047317164
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-02-03 15:11:48 +0100

    fetch api with axios and display on app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const url = 'https://api.adviceslip.com/advice';
 
 function App() {
+  const [quotes, setQuotes] = useState(null);
+  const [value, setValue] = useState('random quote');
+  const [number, setNumber] = useState('1');
+  async function getquote() {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      console.log(response.data.slip.advice);
+      setQuotes(response.data.slip.advice);
+      setNumber(response.data.slip.id);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  getquote();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <p>Advice # {number}</p>
+      <h2>"{quotes}"</h2>
+    </section>
   );
 }
 
